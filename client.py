9e149bc8cc818c4e26197a2717126d4a57d1aebf
arunitasahu <100149243+arunitasahu@users.noreply.github.com>
2022-12-25 22:38:09 +0530

    hello

--- client.py
+++ client.py
@@ -35,7 +35,7 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = (bid_price + ask_price)/2
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
@@ -44,7 +44,7 @@ def getRatio(price_a, price_b):
     """ ------------- Update this function ------------- """
     """ Also create some unit tests for this function in client_test.py"""
     if(price_b==0):
-        #wheb price_b is 0 avoid throwing ZeroDivisionError
+        #when price_b is 0 avoid throwing ZeroDivisionError
         return
     return price_a/price_b
 
@@ -52,12 +52,13 @@ def getRatio(price_a, price_b):
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
-    for _ in range(N):
+    for _ in (range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock]=price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
         print("Ratio %s" % (getRatio(prices['ABC'], prices['DEF'])))
